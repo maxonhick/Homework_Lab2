38d7a86bed04b2de188636abded0fc6fca85c8b3
maxonhick <max2007varlamov@gmail.com>
2025-03-02 13:45:03 +0300

    add comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-int main(){
-	std::cout << "Введите имя пользователя: ";
-	std::string name;
-	std::getline(std::cin, name);
-	std::cout << "Hello world from " << name <<  std::endl;
-	return 0;
+int main(){ // Главная функция программы
+	std::cout << "Введите имя пользователя: "; // Сообщение для ползьователя
+	std::string name; // Создаём переменную для имени
+	std::getline(std::cin, name); // Считываем введённоу имя
+	std::cout << "Hello world from " << name <<  std::endl; // Вывод
+	return 0; // Завершаем программу
 }
