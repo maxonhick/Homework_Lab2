89e85dc5575729d8073f6bd3eb360f81d3efb3a2
maxonhick <max2007varlamov@gmail.com>
2025-03-02 13:29:06 +0300

    vers. without 'using namespace std;'

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-using namespace std;
+
 int main(){
-	cout << "Введите имя пользователя: ";
-	string name;
-	getline(cin, name);
-	cout << "Hello world from " << name <<  endl;
+	std::cout << "Введите имя пользователя: ";
+	std::string name;
+	std::getline(std::cin, name);
+	std::cout << "Hello world from " << name <<  std::endl;
 	return 0;
 }
