985ab97f634c6a9d923989f47f702cf980edda87
maxonhick <max2007varlamov@gmail.com>
2025-03-02 11:19:18 +0300

    modified hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(){
-	cout << "Hello world!" << endl;
+	cout << "Введите имя пользователя: ";
+	string name;
+	getline(cin, name);
+	cout << "Hello world from " << name <<  endl;
 	return 0;
 }
